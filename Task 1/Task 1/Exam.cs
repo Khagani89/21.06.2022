c0a0ee7ed152a0bde9291cbda278d6dc71f5fc53
Khagani89 <khaganichh@code.edu.az>
2022-06-22 11:36:30 +0400

    project

---
+++ Task 1/Task 1/Exam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    internal class Exam
+    {
+        public string Student;
+        public int Point;
+        public string Subject;
+        public int StartDate;
+        public int EndDate;
+    
+    
+    }
+}
---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> examlist = new List<string>();
+
+            
+            
+            
+
+            
+            
+
+
+
+        }
+    }
+
+}
